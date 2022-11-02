fa81905bc45636cbb5f384fb87436150036cd966
Shovan Ghosh <shovanghosh84@gmail.com>
2022-11-02 23:48:28 +0530

    added functionality to index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-ReactDOM.render(<h1>Hello world!</h1>,document.getElementById('root'));
+const your="your lucky number";
+const luckyno=Math.round((Math.floor(Math.random() * 11)+Math.floor(Math.random() * 100) + 1-Math.floor(Math.random() * 11))/Math.floor(Math.random() * 11));
+ReactDOM.render(<div><h1 className="font1">Hello {your}!</h1>
+<p className="heading">
+    {luckyno}
+</p>
+</div>,
+document.getElementById('root'));
